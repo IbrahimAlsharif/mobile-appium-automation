b6d14dfe500c38c1836c8535aefef556f6e22435
NoorKhaled95 <69011426+NoorKhaled95@users.noreply.github.com>
2023-04-05 09:21:17 +0300

    window dimension

--- src/test/java/Web/HomePageTest.java
+++ src/test/java/Web/HomePageTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 public class HomePageTest {
     @Test
     public void startButtonIsDisplayed(){
+
         Assert.assertTrue(HomePage.startButtonIsDisplayed());
     }
 }
