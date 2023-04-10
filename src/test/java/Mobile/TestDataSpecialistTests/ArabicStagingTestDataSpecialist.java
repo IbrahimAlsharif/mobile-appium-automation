258dec4d9660e0592721a0d278a4a48c17e4d8f0
MohDaqqa <mohammed.b.daqqa@gmail.com>
2023-04-10 16:52:01 +0200

    Add Tests for SignUp Screen

--- src/test/java/Mobile/TestDataSpecialistTests/ArabicStagingTestDataSpecialist.java
+++ src/test/java/Mobile/TestDataSpecialistTests/ArabicStagingTestDataSpecialist.java
@@ -21,4 +21,54 @@ public class ArabicStagingTestDataSpecialist implements AbstractTestData{
     public String getSignUpButtonInWelcomeScreen() {
         return null;
     }
+
+    @Override
+    public String getTitleInSignUpScreen() {
+        return null;
+    }
+
+    @Override
+    public String getMainTextInSignUpScreen() {
+        return null;
+    }
+
+    @Override
+    public String getDescriptionTextInSignUpScreen() {
+        return null;
+    }
+
+    @Override
+    public String getRegisterTextInSignUpScreen() {
+        return null;
+    }
+
+    @Override
+    public String getHaveAccountTextInSignUpScreen() {
+        return null;
+    }
+
+    @Override
+    public String getLoginLinkedTextInSignUpScreen() {
+        return null;
+    }
+
+    @Override
+    public String getBottomBarTextInSignUpScreen() {
+        return null;
+    }
+
+    @Override
+    public String getPrivacyPolicyTextInSignUpScreen() {
+        return null;
+    }
+
+    @Override
+    public String getAndTextInSignUpScreen() {
+        return null;
+    }
+
+    @Override
+    public String getTermsUseTextInSignUpScreen() {
+        return null;
+    }
 }
