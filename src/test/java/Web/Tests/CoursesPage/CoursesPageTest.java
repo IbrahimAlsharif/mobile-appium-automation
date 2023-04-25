5b8f544d9dbf9a887a05a7ec1751ca61347d03de
Riham-Tameem <riham.2017209@gmail.com>
2023-04-25 14:35:56 +0200

    add coding: utf-8 on test file

--- src/test/java/Web/Tests/CoursesPage/CoursesPageTest.java
+++ src/test/java/Web/Tests/CoursesPage/CoursesPageTest.java
@@ -1,4 +1,5 @@
 package Web.Tests.CoursesPage;
+// -*- coding: utf-8 -*-
 
 import Base.WebSetup.WebFinder;
 import TestRail.APIException;
--- src/test/java/Web/Tests/Footer/FooterTest.java
+++ src/test/java/Web/Tests/Footer/FooterTest.java
@@ -1,4 +1,5 @@
 package Web.Tests.Footer;
+// -*- coding: utf-8 -*-
 
 import Base.WebSetup.WebFinder;
 import TestRail.APIException;
